ed524f9a22ecda8ee097bbfe2b0bb23bde1aada9
MatildaBjorken <matilda.bjork@hyperisland.se>
2021-04-08 15:03:34 +0200

    basic styling and logic

---
+++ src/components/break.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function BreakInterval(){
+return(
+    <div>
+        <button>-</button>
+        <p>5min</p>
+        <button>+</button>
+    </div>
+)
+}
+export default BreakInterval
--- src/components/timer.js
+++ src/components/timer.js
@@ -1,21 +1,83 @@
 import { useState, useEffect } from 'react';
+import Break from './break';
+import TicTac from '../images/circle-text.svg';
+import getItDone from '../images/getitdone.svg';
 
 function Timer() {
-  const [minutes, setMinutes] = useState(25);
-  const [secounds, setSecounds] = useState(0);
-  const [displayMessage, setDisplayMessage] = useState(false);
+  const [sessionLength, setSessionLength] = useState(25);
+  const [breakLength, setBreakLength] = useState(5);
+  const [timerLabel, setTimerLabel] = useState('Work');
+  const [seconds, setSeconds] = useState(25 * 60);
+  const [timerRunning, setTimerRunning] = useState(false);
 
-  useEffect(() => {}, [secounds]);
+  let min = Math.floor(seconds / 60);
+  let sec = seconds % 60;
 
-  const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  useEffect(() => {
+    const handleSwitch = () => {
+      if (timerLabel === 'Work') {
+        setTimerLabel('Break');
+        // update the break
+        setSeconds(breakLength * 60);
+      } else if (timerLabel === 'Break') {
+        setTimerLabel('Work');
+        //update the session
+        setSeconds(sessionLength * 60);
+      }
+    };
+
+    let countdown = null;
+    if (timerRunning && seconds > 0) {
+      countdown = setInterval(() => {
+        setSeconds(seconds - 1);
+      }, 1000);
+    } else if (timerRunning && seconds === 0) {
+      countdown = setInterval(() => {
+        setSeconds(seconds - 1);
+      }, 1000);
+      handleSwitch();
+    } else {
+      clearInterval(countdown);
+    }
+    return () => clearInterval(countdown);
+  }, [timerRunning, seconds, timerLabel, breakLength, sessionLength]);
+
+  const handleStart = () => {
+    setTimerRunning(true);
+  };
+
+  const handleStop = () => {
+    setTimerRunning(false);
+  };
+  const handleReset = () => {
+    setSeconds(25 * 60);
+    setTimerLabel('Work');
+    setTimerRunning(false);
+  };
+
+  const timerMinutes = min < 10 ? `${min}` : min;
+  const timerSeconds = sec < 10 ? `0${sec}` : sec;
 
   return (
-    <div>
-      <div>
-        <div>bresk time! new session starts in :</div>
-      </div>
-      <div>
-        {minutes}:{secounds}
+    <div className="">
+      <div className="timer-container">
+        <div className="circle">
+          <img src={TicTac} className="ticktac" />
+          <h2 id="timer-label">{timerLabel}</h2>
+          <h3 id="timer-digits">
+            {timerMinutes}:{timerSeconds}
+          </h3>
+        </div>
+
+        <button
+          id="start_stop"
+          onClick={timerRunning ? handleStop : handleStart}
+        >
+          Start/Stop
+        </button>
+        <button onClick={handleReset} id="reset">
+          Reset
+        </button>
       </div>
     </div>
   );
